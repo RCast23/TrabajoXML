c7cba1b4dd8c8f7f6f0d0320d5bc1dc62c7f587e
RCast23 <RobertoCastillaSenero@gmail.com>
2023-11-21 17:44:02 +0100

    Prettify np funciona

--- paquete/GestorVehiculo.py
+++ paquete/GestorVehiculo.py
@@ -1,32 +1,29 @@
 import xml.etree.ElementTree as ET
 from paquete.BaseDeDatos import guardarArchivo
 
-def crear(agenda):
-    scan=input("dame una matricula")#que llame a utiles con un escaner  que verifique
-    vehiculo = ET.SubElement(agenda,'Vehiculo',{'atr1':scan})
+def crear(rootVehiculo):
+    scan=input("dame el ID")#que llame a utiles con un escaner  que verifique
+    vehiculo = ET.SubElement(rootVehiculo,'Vehiculo',{'vehiculoID':scan})
     
     matricula = ET.SubElement(vehiculo, 'Matricula')
     scan=input("dame una matricula")#que llame a utiles con un escaner  que verifique
-    matricula.text =scan
+    matricula.text = scan
     
     marcaYmodelo = ET.SubElement(vehiculo, 'Marca y Modelo')
     scan=input("dame una marca y modela")#que llame a utiles con un escaner  que verifique
-    marcaYmodelo.text =scan
+    marcaYmodelo.text = scan
     
     annoDeFabricacion = ET.SubElement(vehiculo, 'Anno De Fabricacion')
     scan=input("dame un Anno De Fabricacion")#que llame a utiles con un escaner  que verifique
-    annoDeFabricacion.text =scan
+    annoDeFabricacion.text = scan
     
     tarifa = ET.SubElement(vehiculo, 'Tarifa')
     scan=input("dame una tarifa por dia")#que llame a utiles con un escaner  que verifique
-    tarifa.text =scan
+    tarifa.text = scan
     
     estadoVehiculo = ET.SubElement(vehiculo, 'Estado Vehiculo')
     scan=input("dame un estado del vehiculo")#que llame a utiles con un escaner  que verifique
-    estadoVehiculo.text =scan
-    
-    #top.append(emp1) al mandar el root no hace falta 
-    guardarArchivo(agenda)
+    estadoVehiculo.text = scan
     
 def borrar(agenda): #Requiere confirmacion
     empresa = buscar(agenda)
@@ -38,14 +35,14 @@ def borrar(agenda): #Requiere confirmacion
             print("Empresa eliminada")
 
 def modificar(agenda): #Requiere confirmacion
-    empresa = buscar(agenda)
+    empresa = buscarVehiculo(agenda)
     if(empresa!=None):
         mostrarTodos(empresa)
         empresa[0].text=input("Introduzca el nuevo nombre de la empresa")
         empresa[1].text=input("Introduzca el nuevo numero de empleados de la empresa")
         empresa[2].text=input("Introduzca la nueva facturacion de la empresa")
     
-def buscar(agenda):
+def buscarVehiculo(agenda):
     nom = input("Introduzca el nombre de la empresa")
     i=0;
     try:
