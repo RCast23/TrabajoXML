42bf32747967226764c6d3e6019aab98d0ce49bd
111miguel111 <miguelmora111pro@gmail.com>
2023-11-22 13:52:45 +0100

    gestoralquileres falta nada creo

--- paquete/GestorAlquiler.py
+++ paquete/GestorAlquiler.py
@@ -10,12 +10,13 @@ def crear(alquileres):
     alquiler = ET.Element('Alquiler',{'alquilerID':atributo})
         
     #-----------------------------------------
-    print("Dame un ID de vehiculo") 
+    print("Dame una matricula de vehiculo") 
     scan=Utiles.escanerMatricula()
     if(scan!=None and continuar): 
-        if(GestorVehiculo.buscarMatricula(scan, MainXML.root[0])!=None):
+        nodo=GestorVehiculo.buscarMatricula(scan, MainXML.root[0])
+        if(nodo!=None):
             idVehiculo = ET.SubElement(alquiler, 'ID_Vehiculo')
-            idVehiculo.text =scan
+            idVehiculo.text =nodo.attrib['vehiculoID']
         else:
             continuar=False
             print ('No existe ese vehiculo')
@@ -64,90 +65,143 @@ def crear(alquileres):
     return 0
 
 def modificar(root):
-    if(Utiles.validacionIDAlquiler()):
-        nodo=buscarPosicion(top,vehiculo,dni)
-        continuar=True
-        while(continuar):
-            if(nodo!=None):
-                # por si aaso crear una copia de los datos originales y cambiar el nombre a todos los campos
-                
-                print("Introcuce el campo que quieres modificar\n1 ID Alquiler\n2 ID Vehiculo\n3 ID Cliente\n4 Fecha Inicio\n5 Fecha Devolucion\n6 Kilometraje Inicial\n7 Kilometraje Final\n8 precio Final\n9 Recargo\n0 Salir")
-                opcion=Utiles.escanerNumerico()
-                if(opcion=="1"):
-                    scan=Utiles.escanerNumerico()
-                    if(scan!=None):
-                        root[nodo].attrib['alquilerID']=scan
-                elif(opcion=="2"):
-                    scan=Utiles.escanerMatricula()
-                    if(scan!=None):
-                        root[nodo][0].text=scan
-                elif(opcion=="3"):
-                    scan=Utiles.escanerDni()
-                    if(scan!=None):
-                        root[nodo][1].text=scan
-                elif(opcion=="4"):
-                    scan=Utiles.escanerFecha()
-                    if(scan!=None):
-                        root[nodo][2].text=scan
-                elif(opcion=="5"):
-                    scan=Utiles.escanerFecha()
-                    if(scan!=None):
-                        root[nodo][3].text=scan
-                elif(opcion=="6"):
-                    scan=Utiles.escanerNumericoDecimal()
-                    if(scan!=None):
-                        root[nodo][4].text=scan
-                elif(opcion=="7"):
-                    scan=Utiles.escanerNumericoDecimal()
-                    if(scan!=None):
-                        root[nodo][5].text=scan
-                elif(opcion=="8"):
-                    scan=Utiles.escanerNumericoDecimal()
-                    if(scan!=None):
-                        root[nodo][6].text=scan
-                elif(opcion=="9"):
-                    scan=Utiles.escanerNumericoDecimal()
-                    if(scan!=None):
-                        root[nodo][7].text=scan
-                elif(opcion=="0"):
-                    continuar=False
-                else:
-                    print("Escribe un numero dentro de las opciones posibles")
-                
+    print("Introduce la matricula del vehiculo que quieres ver")
+    vehiculo=Utiles.escanerMatricula()
+    if(vehiculo!=None):
+        print("Introduce el dni del cliente que quieres ver")
+        dni=Utiles.escanerDni()
+        if(dni!=None):
+            nodo=buscarPosicionMatriculaDni(top,vehiculo,dni)
+    continuar=True
+    while(continuar):
+        if(nodo!=None):
+            idAlquiler
+            idVehiculo
+            idCliente
+            fechaInicio
+            fechaDevolucion
+            kilometrajeInicial
+            kilometrajeFinal
+            precioFinal
+            recargo
+            
+            print("Introcuce el campo que quieres modificar\n1 ID Alquiler\n2 ID Vehiculo\n3 ID Cliente\n4 Fecha Inicio\n5 Fecha Devolucion\n6 Kilometraje Inicial\n7 Kilometraje Final\n8 precio Final\n9 Recargo\n0 Salir")
+            opcion=Utiles.escanerNumerico()
+            if(opcion=="1"):
+                scan=Utiles.escanerNumerico()#-----------------------------------------------
+                if(scan!=None):
+                    if(buscarPosicionId(alquileres,scan)==None):
+                        idAlquiler=scan
+            elif(opcion=="2"):
+                print("Dame una matricula de vehiculo") 
+                scan=Utiles.escanerMatricula()
+                if(scan!=None): 
+                    nodo=GestorVehiculo.buscarMatricula(scan, MainXML.root[0])
+                    if(nodo!=None):
+                        idVehiculo =nodo.attrib['vehiculoID']
+                    else:
+                        print ('No existe ese vehiculo')
+            elif(opcion=="3"):
+                scan=Utiles.escanerDni()
+                if(scan!=None):
+                    idCliente=scan
+            elif(opcion=="4"):
+                scan=Utiles.escanerFecha()
+                if(scan!=None):
+                    fechaInicio=scan
+            elif(opcion=="5"):
+                scan=Utiles.escanerFecha()
+                if(scan!=None):
+                    fechaDevolucion=scan
+            elif(opcion=="6"):
+                scan=Utiles.escanerNumericoDecimal()
+                if(scan!=None):
+                    kilometrajeInicial=scan
+            elif(opcion=="7"):
+                scan=Utiles.escanerNumericoDecimal()
+                if(scan!=None):
+                    kilometrajeFinal=scan
+            elif(opcion=="8"):
+                scan=Utiles.escanerNumericoDecimal()
+                if(scan!=None):
+                    precioFinal=scan
+            elif(opcion=="9"):
+                scan=Utiles.escanerNumericoDecimal()
+                if(scan!=None):
+                    recargo=scan
+            elif(opcion=="0"):
+                continuar=False
             else:
-                print("No encontrado ningun alquiler con la matricula de ese coche y ese dni de cliente")
-            print("Quieres guardar los cambios realizados?")
-            if(Utiles.confirmacion()):
-                #Preguntar a roberto como carajos guardo algo
-                BaseDeDatos.guardar(top)
+                print("Escribe un numero dentro de las opciones posibles")
+            
+        else:
+            print("No encontrado ningun alquiler con la matricula de ese coche y ese dni de cliente")
+            continuar=False
+    if(nodo!=None):
+        print("Quieres guardar los cambios realizados?")
+        if(Utiles.confirmacion()):
+            if(idAlquiler != None):
+                root[nodo].attrib['alquilerID']=idAlquiler
+            if(idVehiculo!=None):
+                root[nodo][0].text=idVehiculo
+            if(idCliente!=None):
+                root[nodo][1].text=idCliente
+            if(fechaInicio!=None):
+                root[nodo][2].text=fechaInicio
+            if(fechaDevolucion!=None):
+                root[nodo][3].text=fechaDevolucion
+            if(kilometrajeInicial!=None):
+                root[nodo][4].text=kilometrajeInicial
+            if(kilometrajeFinal!=None):
+                root[nodo][5].text=kilometrajeFinal
+            if(precioFinal!=None):
+                root[nodo][6].text=precioFinal
+            if(recargo!=None):
+                root[nodo][7].text=recargo
     return 0
 def buscarMostrarTodosVehiculo(alquileres):
-    texto=input("Introduce el id del coche que quieres ver los alquileres")
-    for x in alquileres:
-        if(x[0].text.lower()==texto.lower()):
-            print("He ecnontrado: ",x.text)
-            Utiles.recorrer(x)
+    print("Introduce la matricula de los alquileres que quieres ver")
+    vehiculo=Utiles.escanerMatricula()
+    if(vehiculo!=None):
+        ninguno=True
+        for x in alquileres:
+            if(x[0].text.lower()==texto.lower()):
+                print("He ecnontrado: ",x.text)
+                Utiles.recorrer(x)
+                ninguno=False
+        if(ninguno):
+                print("No se ha encontrado ningun alquiler con esa matricula")
     return 0
 def buscarMostrarTodosDni(alquileres):
-    texto=input("Introduce el id del cliente que quieres ver los alquileres")
-    for x in alquileres:
-        if(x[1].text.lower()==texto.lower()):
-            print("He ecnontrado: ",x.text)
-            Utiles.recorrer(x)
+    print("Introduce el dni de los alquileres que quieres ver")
+    dni=Utiles.escanerDni()
+    if(dni!=None):
+        ninguno=True
+        for x in alquileres:
+            if(x[1].text.lower()==texto.lower()):
+                print("He ecnontrado: ",x.text)
+                Utiles.recorrer(x)
+                ninguno=False
+        if(ninguno):
+                print("No se ha encontrado ningun alquiler con ese Dni")
     return 0
-def buscarMostrar(alquileres):
-    
-    vehiculo=input("Introduce la matricula del vehiculo que quieres ver")
-    dni=input("Introduce el dni del cliente que quieres ver")
-    
-    for x in alquileres:
-        if( x[0].text.lower()==vehiculo.lower() and x[1].text.lower()==dni.lower()):
-            print("He ecnontrado: ",x.text)
-            Utiles.recorrer(x)
+def buscarMostrarMatriculaDni(alquileres):
+    print("Introduce la matricula del vehiculo que quieres ver")
+    vehiculo=Utiles.escanerMatricula()
+    if(vehiculo!=None):
+        print("Introduce el dni del cliente que quieres ver")
+        dni=Utiles.escanerDni()
+        if(dni!=None):
+            ninguno=True
+            for x in alquileres:
+                if( x[0].text.lower()==vehiculo.lower() and x[1].text.lower()==dni.lower()):
+                    print("He ecnontrado: ",x.text)
+                    Utiles.recorrer(x)
+                    ninguno=False
+            if(ninguno):
+                print("No se ha encontrado ningun alquiler con esa Matricula y Dni")
     return 0
-def buscarPosicion(alquileres,vehiculo,dni):
-    #reacer buscando por matricula y dni
-    #con una tupla , tiene que permitir elegir un
+def buscarPosicionMatriculaDni(alquileres,vehiculo,dni):
     cont=0
     opciones=[]
     for x in alquileres:
@@ -167,6 +221,15 @@ def buscarPosicion(alquileres,vehiculo,dni):
                 Utiles.recorrer(x)
                 return nodo
             nodo+=1
+    return None
+def buscarPosicionId(alquileres,id):
+    nodo=0
+    for x in alquileres:
+        if(x.attrib['alquilerID'].lower()==id):
+            print(cont,"-He ecnontrado: ",x.text)
+            Utiles.recorrer(x)
+            return nodo
+        nodo+=1
     return None
 def mostrar(root):
     Utiles.recorrer(root)
